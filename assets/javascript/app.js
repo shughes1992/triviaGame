c1c1ae1ef0f8b4ded43e4f8d0354b30604561366
Samantha Hughes <hughes2@law.upenn.edu>
2018-03-08 17:35:52 -0500

    adding decremental timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,35 @@
-var questions = [
+//questions to be asked
+var number = 45;
+var intervalId;
+var wins = 0;
+var losses = 0;
+var correct =0;
+var incorrect =0;
+
+
+
+  //  When the stop button gets clicked, run the stop function.
+  $("#done").on("click", stop());
+
+  //  When the resume button gets clicked, execute the run function.
+  $("#start").on("click", run());
+
+  
+function run() {
+  clearInterval(intervalId);
+  intervalId = setInterval(decrement, 1000);
+}
+
+
+function stop() {
+
+  clearInterval(intervalId);
+  // # of correct/ incorrect answers are calculated and appended to "correct" or "incorrect" variable
+}
+
+//I would like to for loop through the following questions and have them appear in the "Questions" panel
+
+var questionList = [
     {
       Q: 'Who makes the best vegan cheesesteak?',
       A: ['Pats & Genos', 'McDonalds', 'Raj', 'WizKid'],
@@ -7,4 +38,34 @@ var questions = [
       Q: 'Where is the best place to dance?',
       A: ['In my living room', '700 Club', 'Raw Sushi', 'Anywhere'],
       correctAnswer: '700 Club'
-    }, 
+    }
+
+]
+
+function decrement() {
+
+  //  Decrease number by one.
+  number--;
+
+  //  Show the number in the #show-number tag.
+  $("#display").html("<h3>" + number + "</h3>");
+
+  //  Once number hits zero...
+  if (number === 0) {
+
+    //  ...run the stop function.
+    stop();
+
+    //  Alert the user that time is up.
+    alert("Time's Up!");
+  }
+}
+
+
+//if user answers all questions correctly, then wins++
+
+
+//else losses++
+
+
+
