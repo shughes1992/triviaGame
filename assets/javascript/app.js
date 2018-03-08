14ce10bf9b37b2369408d113946013426d016596
Samantha Hughes <hughes2@law.upenn.edu>
2018-03-07 20:12:29 -0500

    adding all necessary styling and html

---
+++ assets/javascript/app.js
@@ -0,0 +1,35 @@
+var questions = [
+    {
+      Q: 'Who makes the best vegan cheesesteak?',
+      A: ['Pats & Genos', 'McDonalds', 'Raj', 'WizKid'],
+      correctAnswer: 'WizKid'
+    }, {
+      Q: 'When did the Liberty Bell get its name?',
+      A: ['when it was made, in 1701', 'when it rang on July 4, 1776', 'in the 19th century when it became a symbol of the abolition of slavery', 'none of the above'],
+      correctAnswer: 'in the 19th century when it became a symbol of the abolition of slavery'
+    }, {
+      Q: 'In the Roy Rogers-Dale Evans Museum, you will find Roy and Dales stuffed horses. Roy\'s horse was named Trigger, which was Dales horse?',
+      A: ['Buttermilk', 'Daisy', 'Scout', 'Tulip'],
+      correctAnswer: 'Buttermilk'
+    }, {
+      Q: 'The Daniel Boon museum at the home where he died can best be described how?',
+      answers: ['a log cabin in Kentucky', 'a two-story clapboard house in Tennessee', 'a four-story Georgian-style home in Missouri', 'a three story brick house in Arkansas'],
+      correctAnswer: 'a four-story Georgian-style home in Missouri'
+    }, {
+      Q: 'Which of the following items was owned by the fewest U.S. homes in 1990?',
+      answers: ['home computer', 'compact disk player', 'cordless phone', 'dishwasher'],
+      correctAnswer: 'compact disk player'
+    }, {
+      Q: 'Who holds the record for the most victories in a row on the professional golf tour?',
+      answers: ['Jack Nicklaus', 'Arnold Palmer', 'Byron Nelson', 'Ben Hogan'],
+      correctAnswer: 'Byron Nelson'
+    }, {
+      Q: 'Who is third behind Hank Aaron and Babe Ruth in major league career home runs?',
+      A: ['Reggie Jackson', 'Harmon Killebrew', 'Willie Mays', 'Frank Robinson'],
+      correctAnswer: 'Willie Mays'
+    }, {
+      Q: 'In 1990, in what percentage of U.S. married couples did the wife earn more money than the husband',
+      A: ['8', '18', '38', '58'],
+      correctAnswer: '18'
+    }
+  ];
